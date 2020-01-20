be32b6dc0b7bd25e7480f4676ecb4cae6eef29eb
Nemolite <g25092011@mail.ru>
2020-01-20 20:21:38 +0300

    fixing the script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+ $(document).ready(function(){
+
+   $(".menu__icon")
+       .click(
+          function()
+          {
+          	 $(".menu__links").toggle(600);
+           }
+        );
+ 
+});
